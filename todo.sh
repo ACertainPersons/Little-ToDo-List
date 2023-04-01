cfa7f4f85c4ccc39ee12843088ee0dca29a68ab9
ACertainPersons <127463096+ACertainPersons@users.noreply.github.com>
2023-04-01 14:31:06 -0500

    stupid bug fixed

--- todo.sh
+++ todo.sh
@@ -15,7 +15,7 @@ then
    /usr/bin/python3 /Users/amos/Shell/Little-ToDo-List/todo-a.py
 fi
 
-if [ "$1" != "-v" ] && [ "$1" != "-l" ] && [ ! -z "$1" ] && [ "$1" == "-a" ];
+if [ "$1" != "-v" ] && [ "$1" != "-l" ] && [ ! -z "$1" ] && [ "$1" != "-a" ];
 then
    echo "invalid"
 fi
